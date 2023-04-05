b27bd94159d4b96a8d7cba2dc5b2ddf5c14720a6
Wasim <wasim.rashid@developers.studio>
2023-04-05 12:37:50 +0500

    add card

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import {FiThumbsUp} from "react-icons/fi";
+
+export default function Card({ movie }: { movie: any }) {
+  return (
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+      <Link href={`movies/${movie.id}`}>
+        <Image width={500} height={300} src={`https://image.tmdb.org/t/p/original/${
+            movie.backdrop_path || movie.poster_path
+          }`} alt='movie-image' placeholder='blur' blurDataURL='/images/loader.svg'></Image>
+          <div className="p-2">
+          <p className=" line-clamp-2">{movie.overview}</p>
+          <h2 className=" font-bold">
+            {movie.title || movie.name}
+          </h2>
+          <p className=" flex items-center">
+            {movie.release_date || movie.first_air_date}
+            <FiThumbsUp className='h-5 mr-1 ml-3'></FiThumbsUp>{movie.vote_count}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/app/components/Results.tsx
+++ src/app/components/Results.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Card from './Card'
 
 export default function Results({movies}:{movies:any}) {
+    console.log(movies);
     return (
-        <div className="">
+        <div className=" grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
           {movies.results.map((movie:any) => (
-            <div key={movie.id}>{movie.title} - {movie.id}</div>
+            <Card key={movie.id} movie={movie}></Card>
           ))}
         </div>
       )
